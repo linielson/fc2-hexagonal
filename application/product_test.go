13a4f6c91dfad049af816cef8f6d2c764b53235d
Linielson <linielsonrosa@gmail.com.br>
2021-04-05 07:47:11 -0300

    Create product interface

---
+++ application/product_test.go
@@ -0,0 +1,89 @@
+package application_test
+
+import (
+	"github.com/linielson/fc2-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
+
+func TestProduct_GetID(t *testing.T) {
+	product := application.Product{}
+	id := uuid.NewV4().String()
+	product.ID = id
+
+	require.Equal(t, id, product.ID)
+}
+
+
+func TestProduct_GetName(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+
+	require.Equal(t, "Hello", product.Name)
+}
+
+func TestProduct_GetPrice(t *testing.T) {
+	product := application.Product{}
+	product.Price = 10
+
+	require.Equal(t, 10.0, product.Price)
+}
+
+func TestProduct_GetStatus(t *testing.T) {
+	product := application.Product{}
+	product.Status = application.DISABLED
+
+	require.Equal(t, application.DISABLED, product.Status)
+}
